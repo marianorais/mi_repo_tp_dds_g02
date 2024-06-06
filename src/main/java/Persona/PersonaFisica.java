fd365e70ee38b845f2fffd9949667f6c35ec43ee
Lord-Bardo <mlombardo@frba.utn.edu.ar>
2024-06-06 16:25:37 -0300

    Rediseño e implementacion de colabs, faltan test y hay un par de todos, adems de lo de canjearPuntos

--- src/main/java/Persona/PersonaFisica.java
+++ src/main/java/Persona/PersonaFisica.java
@@ -1,5 +1,7 @@
 package Persona;
 
+import Colaboradores.Colaboracion;
+import Colaboradores.TipoColaboracion;
 import Formularios.Formulario;
 
 import java.util.ArrayList;
@@ -8,9 +10,24 @@ import java.util.List;
 public class PersonaFisica {
     private List<Formulario> formulariosRespondidos;
 
+    private List<Colaboracion> colaboracionesRealizadas;
+    private List<TipoColaboracion> colaboracionesPermitidas;
+
     public PersonaFisica(Formulario formulario) {
         this.formulariosRespondidos = new ArrayList<>();
         this.formulariosRespondidos.add(formulario);
+    }
+
+    public double calcularPuntaje(){
+        return colaboracionesRealizadas.stream()
+                                        .mapToDouble(colaboracion->colaboracion.calcularPuntaje())
+                                        .sum();
+    }
+    public void registrarColaboracion(){
+    //TODO
+    }
+    public void canjearPuntos(){
+        //TODO
     }
     public void agregarFormulario(Formulario formulario){
         this.formulariosRespondidos.add(formulario);
--- src/main/java/Persona/PersonaJuridica.java
+++ src/main/java/Persona/PersonaJuridica.java
@@ -1,14 +1,14 @@
 package Persona;
 
-import Colaboradores.Colaborador;
-import DatosDePersonas.*;
+import Colaboradores.Colaboracion;
+import Colaboradores.TipoColaboracion;
 import Formularios.Formulario;
 import Usuario.Usuario;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonaJuridica extends Colaborador {
+public class PersonaJuridica{
 
     /* FALTAN ESTOS ATRIBUTOS
     - heladerasACargo : List<Heladera>
@@ -16,15 +16,27 @@ public class PersonaJuridica extends Colaborador {
     private Usuario usuario;
     private List<Formulario> formulariosRespondidos;
 
+    private List<Colaboracion> colaboracionesRealizadas;
+    private List<TipoColaboracion> colaboracionesPermitidas;
+
     public PersonaJuridica(Formulario formulario) {
         this.formulariosRespondidos = new ArrayList<>();
         this.formulariosRespondidos.add(formulario);
     }
-
+    public double calcularPuntaje(){
+        return colaboracionesRealizadas.stream()
+                .mapToDouble(colaboracion->colaboracion.calcularPuntaje())
+                .sum();
+    }
     public Oferta ofrecerProductos(){
         return new Oferta();
     }
-
+    public void registrarColaboracion(){
+        //TODO
+    }
+    public void canjearPuntos(){
+        //TODO
+    }
     public void agregarFormulario(Formulario formulario){
         this.formulariosRespondidos.add(formulario);
     }
